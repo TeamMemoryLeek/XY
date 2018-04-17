a10209acc81d210ace4b41798a326509bf3bdcbf
Sebastian Kylander <sebastian@kylander.me>
2018-04-17 22:12:04 +0200

    Make window title parameter wide string

--- src/code/Window.cpp
+++ src/code/Window.cpp
@@ -5,7 +5,7 @@
 namespace xy
 {
 
-Window::Window(uint32_t width, uint32_t height, const std::string& title)
+Window::Window(uint32_t width, uint32_t height, const std::wstring& title)
 {
 }
 
--- src/code/Window.h
+++ src/code/Window.h
@@ -7,7 +7,7 @@ namespace xy
 class Window
 {
 public:
-	Window(uint32_t width, uint32_t height, const std::string& title);
+	Window(uint32_t width, uint32_t height, const std::wstring& title);
 	~Window();
 };
 

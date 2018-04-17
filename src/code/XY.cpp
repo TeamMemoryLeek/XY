f96f458e165b45e5bde88d4ab77b937193ebb180
Adam Wallberg <adamwallbergcontact@gmail.com>
2018-04-17 22:31:09 +0200

    Setup global window functions

--- src/code/XY.cpp
+++ src/code/XY.cpp
@@ -1,20 +1,26 @@
 #include "XY.h"
+#include "Canvas.h"
 
 namespace xy
 {
+
+static Window* window = nullptr;
+static Canvas* canvas = nullptr;
 
 Window* createWindow(
 	unsigned int width, 
 	unsigned int height,
 	std::string& title)
 {
-	// TODO: Create and return window
-	return nullptr;
+	canvas = new Canvas(width, height);
+	window = new Window(width, height, title);
+	return window;
 }
 
 void destroyWindow(Window* window)
 {
-	// TODO: Destroy window
+	delete window;
+	delete canvas;
 }
 
 }

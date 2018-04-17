13e524596ef3ae80148b0f1ad97f55f2ad327dce
Sebastian Kylander <sebastian@kylander.me>
2018-04-17 22:12:22 +0200

    Basic window creation

--- src/code/Window.cpp
+++ src/code/Window.cpp
@@ -7,10 +7,27 @@ namespace xy
 
 Window::Window(uint32_t width, uint32_t height, const std::wstring& title)
 {
+	static uint32_t window_uid = 0;
+	std::wstring window_name = L"XYWindow" + std::to_wstring(window_uid++);
+
+	m_hwnd = CreateWindowExW(
+		WS_EX_OVERLAPPEDWINDOW,
+		L"XYWindowClass",
+		window_name.c_str(),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		(int)width, (int)height,
+		nullptr, nullptr,
+		GetModuleHandleW(nullptr),
+		nullptr
+	);
+
+	ShowWindow(m_hwnd, SW_SHOW);
 }
 
 Window::~Window()
 {
+	DestroyWindow(m_hwnd);
 }
 
 }
--- src/code/Window.h
+++ src/code/Window.h
@@ -9,6 +9,9 @@ class Window
 public:
 	Window(uint32_t width, uint32_t height, const std::wstring& title);
 	~Window();
+
+private:
+	HWND m_hwnd;
 };
 
 }

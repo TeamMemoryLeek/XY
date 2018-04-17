151e252c93e0c627058602094be2950baedcd0c5
Sebastian Kylander <sebastian@kylander.me>
2018-04-17 22:36:16 +0200

    Register window class

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -1,14 +1,35 @@
 #include "Window.h"
 
-#include <Windows.h>
-
 namespace xy
 {
 
+LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+	default:
+		return DefWindowProcW(hwnd, msg, wparam, lparam);
+	}
+}
+
+static ATOM registerClass()
+{
+	WNDCLASSEXW desc = {};
+	desc.cbSize        = sizeof(WNDCLASSEXW);
+	desc.style         = CS_HREDRAW | CS_VREDRAW;
+	desc.lpfnWndProc   = wndProc;
+	desc.hInstance     = GetModuleHandle(nullptr);
+	desc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	desc.lpszClassName = L"XYWindowClass";
+
+	return RegisterClassExW(&desc);
+}
+
 Window::Window(uint32_t width, uint32_t height, const std::wstring& title)
 {
-	static uint32_t window_uid = 0;
-	std::wstring window_name = L"XYWindow" + std::to_wstring(window_uid++);
+	static ATOM     window_class = registerClass();
+	static uint32_t window_uid   = 0;
+	std::wstring    window_name  = L"XYWindow" + std::to_wstring(window_uid++);
 
 	m_hwnd = CreateWindowExW(
 		WS_EX_OVERLAPPEDWINDOW,
@@ -18,7 +39,7 @@ Window::Window(uint32_t width, uint32_t height, const std::wstring& title)
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		(int)width, (int)height,
 		nullptr, nullptr,
-		GetModuleHandleW(nullptr),
+		GetModuleHandle(nullptr),
 		nullptr
 	);
 
--- src/core/Window.h
+++ src/core/Window.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 
+#include <Windows.h>
+
 namespace xy
 {
 

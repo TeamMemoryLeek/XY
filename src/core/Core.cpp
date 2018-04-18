8b16a24b65d5e46a4bdcc65e55027ea8aef2a957
Adam Wallberg <adamwallbergcontact@gmail.com>
2018-04-18 13:01:49 +0200

    Draw to window in Core

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -11,20 +11,30 @@ void Core::initialize(
 	const std::wstring& title)
 {
 	Window::initialize(width, height, title);
+	Canvas::initialize(width, height);
 }
 
 void Core::finalize()
 {
+	Canvas::finalize();
 	Window::finalize();
 }
 
 bool Core::shouldRun()
 {
-	return false;
+	return true;
 }
 
 void Core::finalizeRendering()
 {
+	HBITMAP map = CreateBitmap(
+		Canvas::_width, 
+		Canvas::_height, 
+		1, 
+		8 * 4, 
+		(void*)Canvas::_pixels);
+
+	Window::drawToWindow(map);
 }
 
 }
